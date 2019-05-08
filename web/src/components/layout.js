8e130f17c35ea273edc14c356c66b51517344a58
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-05-08 08:36:01 -0700

    update styling

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -25,11 +25,11 @@ const Layout = ({ children, companyInfo, siteTitle, siteSettings }) => {
         <div className={styles.content}>{children}</div>
         <footer className={styles.footer}>
           <div className={styles.footerWrapper}>
-            <div className={styles.companyAddress}>
+            <div className={styles.companyInfo}>
               {companyInfo && (
                 <div>
                   {companyInfo.name}
-                  <br />
+                  {/* <br />
                   {companyInfo.address1}
                   <br />
                   {companyInfo.address2 && (
@@ -39,7 +39,7 @@ const Layout = ({ children, companyInfo, siteTitle, siteSettings }) => {
                     </span>
                   )}
                   {companyInfo.zipCode} {companyInfo.city}
-                  {companyInfo.country && <span>, {companyInfo.country}</span>}
+                  {companyInfo.country && <span>, {companyInfo.country}</span>} */}
                   <ReactSVG
                     className={styles.svgWrapper}
                     src={logo && imageUrlFor(buildImageObj(logo)).url()}
