ca6633310a55956cdf7e3d6f8ffc3af2088adcb5
Christopher Czach <ChrisCzach@ChristophersMBP.socal.rr.com>
2019-05-11 15:40:59 -0700

    base64 image for figure and slideshow not working

--- web/src/components/image.js
+++ web/src/components/image.js
@@ -7,10 +7,15 @@ import { buildImageObj } from '../lib/helpers'
 
 export default function Image({ asset, args, fixed = false, ...props }) {
   const imageArgs =
-    args || (fixed ? { width: 2400, height: Math.floor((9 / 16) * 2400) } : { maxWidth: 2400 })
+    args || (fixed ? { width: 1200, height: Math.floor((9 / 16) * 1200) } : { maxWidth: 1200 })
 
-  const base64 = asset.asset.metadata.lqip || ''
+  let base64 = ''
 
+  try {
+    asset.asset.metadata.lqip
+  } catch (err) {
+    console.log(err)
+  }
   const imgProps = fixed
     ? getFixedGatsbyImage(asset, imageArgs, sanityConfig.api)
     : getFluidGatsbyImage(asset, imageArgs, sanityConfig.api)
