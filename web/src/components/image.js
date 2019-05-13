2e3845409140e1bc4e84b5a6aca1e3bdef354de7
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-05-13 08:04:46 -0700

    fix blur up

--- web/src/components/image.js
+++ web/src/components/image.js
@@ -9,20 +9,22 @@ export default function Image({ asset, args, fixed = false, ...props }) {
   const imageArgs =
     args || (fixed ? { width: 1200, height: Math.floor((9 / 16) * 1200) } : { maxWidth: 1200 })
 
-  let base64 = ''
-
-  try {
-    asset.asset.metadata.lqip
-  } catch (err) {
-    console.log(err)
-  }
   const imgProps = fixed
     ? getFixedGatsbyImage(asset, imageArgs, sanityConfig.api)
     : getFluidGatsbyImage(asset, imageArgs, sanityConfig.api)
   // console.log(imgProps)
+  try {
+    imgProps.base64 = asset.asset.metadata.lqip
+  } catch (err) {
+    console.log(err)
+  }
+
   if (fixed) {
-    return <Img fixed={{ ...imgProps, base64 }} alt={asset.alt} {...props} />
+    return <Img fixed={{ ...imgProps }} alt={asset.alt} {...props} />
   } else {
-    return <Img fluid={{ ...imgProps, base64 }} alt={asset.alt} {...props} />
+    if (imageArgs.maxWidth && imageArgs.maxHeight) {
+      imgProps.aspectRatio = imageArgs.maxWidth / imageArgs.maxHeight
+    }
+    return <Img fluid={{ ...imgProps }} alt={asset.alt} {...props} />
   }
 }
