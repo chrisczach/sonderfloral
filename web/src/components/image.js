a69afe8d807e96229f87b652fbebf3db74945039
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-05-13 10:57:45 -0700

    need to fix logic for aspect ratio

--- web/src/components/image.js
+++ web/src/components/image.js
@@ -8,18 +8,14 @@ import { join } from 'path'
 
 export default function Image({ asset, args, fixed = false, ...props }) {
   // console.log('asset', asset)
-  const urlWithRect = imageUrlFor(asset)
-    .width(args.maxWidth || args.width)
-    .height(args.maxHeight || args.height)
-    .url()
-    .split('?')
-  const url = urlWithRect[0]
-  const rect = urlWithRect[1].split('&')[0]
-  console.log(urlWithRect)
-  console.log(url)
-  console.log(rect)
   const imageArgs =
     args || (fixed ? { width: 1200, height: Math.floor((9 / 16) * 1200) } : { maxWidth: 1200 })
+  const processedImg = imageUrlFor(asset)
+    .width(args.maxWidth || args.width || null)
+    .height(args.maxHeight || args.height || null)
+    .url()
+  const url = processedImg.split('?')[0] + '?'
+  const urlWithRect = processedImg.split('&')[0] + '&'
 
   const imgProps = fixed
     ? getFixedGatsbyImage(asset, imageArgs, sanityConfig.api)
@@ -30,7 +26,12 @@ export default function Image({ asset, args, fixed = false, ...props }) {
   } catch (err) {
     console.log(err)
   }
-  // console.log('imgProps', JSON.stringify(imgProps.srcSet, null, 2))
+
+  const addRect = srcToAddRect => srcToAddRect.split(url).join(urlWithRect)
+  imgProps.src = addRect(imgProps.src)
+  imgProps.srcSet = addRect(imgProps.srcSet)
+  imgProps.srcWebp = addRect(imgProps.srcWebp)
+  imgProps.srcSetWebp = addRect(imgProps.srcSetWebp)
   if (fixed) {
     return <Img fixed={{ ...imgProps }} alt={asset.alt} {...props} />
   } else {
