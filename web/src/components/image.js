b0cb6cf2bd29529a72da2d84393ed42bdb6a6534
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-05-13 09:34:19 -0700

    WIP trying to get hotspot crop

--- web/src/components/image.js
+++ web/src/components/image.js
@@ -4,8 +4,20 @@ import { getFluidGatsbyImage, getFixedGatsbyImage } from 'gatsby-source-sanity'
 import sanityConfig from '../../../studio/sanity.json'
 import { imageUrlFor } from '../lib/image-url'
 import { buildImageObj } from '../lib/helpers'
+import { join } from 'path'
 
 export default function Image({ asset, args, fixed = false, ...props }) {
+  // console.log('asset', asset)
+  const urlWithRect = imageUrlFor(asset)
+    .width(args.maxWidth || args.width)
+    .height(args.maxHeight || args.height)
+    .url()
+    .split('?')
+  const url = urlWithRect[0]
+  const rect = urlWithRect[1].split('&')[0]
+  console.log(urlWithRect)
+  console.log(url)
+  console.log(rect)
   const imageArgs =
     args || (fixed ? { width: 1200, height: Math.floor((9 / 16) * 1200) } : { maxWidth: 1200 })
 
@@ -18,7 +30,7 @@ export default function Image({ asset, args, fixed = false, ...props }) {
   } catch (err) {
     console.log(err)
   }
-
+  // console.log('imgProps', JSON.stringify(imgProps.srcSet, null, 2))
   if (fixed) {
     return <Img fixed={{ ...imgProps }} alt={asset.alt} {...props} />
   } else {
