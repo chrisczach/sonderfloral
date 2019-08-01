97666acd1dfc6836d65103e46af7ecb61bb7540a
Christopher Czach <ChrisCzach@ChristophersMBP.socal.rr.com>
2019-07-31 20:03:34 -0700

    trying to add main image

--- studio/schemas/page.js
+++ studio/schemas/page.js
@@ -3,7 +3,7 @@ export default {
   title: "Page",
   type: "document",
   liveEdit: false,
-  __experimental_actions: ["update", "publish" /*'create', 'delete'*/],
+  __experimental_actions: ["update", "publish" /* 'create', 'delete' */],
   fields: [
     {
       name: "title",
@@ -11,9 +11,24 @@ export default {
       type: "string"
     },
     {
+      name: "mainImage",
+      title: "Main image",
+      type: "mainImage"
+    },
+    {
       name: "body",
       title: "Body",
       type: "blockContent"
     }
-  ]
+  ],
+  preview: {
+    select: {
+      image: "mainImage"
+    },
+    prepare({ image }) {
+      return {
+        media: image
+      };
+    }
+  }
 };
