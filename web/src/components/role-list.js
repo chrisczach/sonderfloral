4139b367bd27f63aa1943b80d22c4d021c329afc
Christopher Czach <ChrisCzach@ChristophersMBP.socal.rr.com>
2019-08-10 12:23:48 -0700

    fix link

--- web/src/components/role-list.js
+++ web/src/components/role-list.js
@@ -13,59 +13,70 @@ function RoleList({ items, title }) {
     <div className={styles.root}>
       <h2 className={styles.headline}>{title}</h2>
       <ul className={styles.list}>
-        {items.map(item => (
-          <li
-            key={item._key}
-            className={styles.listItem}
-            style={{ cursor: item.person.instagram ? 'pointer' : 'auto' }}
-          >
-            <div>
-              <div className={styles.avatar}>
-                {item.person && item.person.image && item.person.image.asset && (
-                  <img
-                    src={imageUrlFor(buildImageObj(item.person.image))
-                      .width(100)
-                      .height(100)
-                      .fit('crop')
-                      .url()}
-                    alt=""
-                  />
-                )}
-              </div>
-            </div>
-            <div className={item.person.instagram ? styles.nameWrapper : styles.nameWrapperNoShare}>
-              <div className={styles.names}>
-                <div>
-                  <strong>{(item.person && item.person.name) || <em>Missing</em>}</strong>
+        {items.map(item =>
+          addLink(item.person.instagram)(
+            <li
+              key={item._key}
+              className={styles.listItem}
+              style={{ cursor: item.person.instagram ? 'pointer' : 'auto' }}
+            >
+              <div>
+                <div className={styles.avatar}>
+                  {item.person && item.person.image && item.person.image.asset && (
+                    <img
+                      src={imageUrlFor(buildImageObj(item.person.image))
+                        .width(100)
+                        .height(100)
+                        .fit('crop')
+                        .url()}
+                      alt=""
+                    />
+                  )}
                 </div>
-                {item.roles && (
+              </div>
+              <div
+                className={item.person.instagram ? styles.nameWrapper : styles.nameWrapperNoShare}
+              >
+                <div className={styles.names}>
                   <div>
-                    {item.roles.map((role, idx) => {
-                      switch (true) {
-                        case idx === 0:
-                          return <span key={role}>{ucfirst(role)}</span>
-                        case idx === item.roles.length - 1:
-                          return <span key={role}> & {role}</span>
-                        default:
-                          return <span key={role}>, {role}</span>
-                      }
-                    })}
+                    <strong>{(item.person && item.person.name) || <em>Missing</em>}</strong>
                   </div>
-                )}
+                  {item.roles && (
+                    <div>
+                      {item.roles.map((role, idx) => {
+                        switch (true) {
+                          case idx === 0:
+                            return <span key={role}>{ucfirst(role)}</span>
+                          case idx === item.roles.length - 1:
+                            return <span key={role}> & {role}</span>
+                          default:
+                            return <span key={role}>, {role}</span>
+                        }
+                      })}
+                    </div>
+                  )}
+                </div>
+                <div className={styles.followLink}>
+                  {item.person.instagram && `Follow ${(item.person && item.person.name) || ''}`}
+                </div>
               </div>
-              <div className={styles.followLink}>
-                {item.person.instagram && (
-                  <a className={styles.link} href={item.person.instagram} target="_blank">
-                    Follow {(item.person && item.person.name) || ''}
-                  </a>
-                )}
-              </div>
-            </div>
-          </li>
-        ))}
+            </li>
+          )
+        )}
       </ul>
     </div>
   )
 }
 
+const addLink = link => element => {
+  if (link) {
+    return (
+      <a href={link} target="_blank" className={styles.link}>
+        {element}
+      </a>
+    )
+  } else {
+    return element
+  }
+}
 export default RoleList
