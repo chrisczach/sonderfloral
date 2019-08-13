94554dcaf0e4e61ec9f03391f6f6bbe48eb72512
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-08-13 13:01:36 -0700

    update 404

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -6,13 +6,12 @@ import Layout from '../containers/layout'
 import { responsiveTitle1 } from '../components/typography.module.css'
 
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <SEO title="404: Not found" />
-    <Container>
-      <h1 className={responsiveTitle1}>Not found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Container>
-  </Layout>
+
+    <h1 className={responsiveTitle1}>Not found</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness. </p>
+  </>
 )
 
 export default NotFoundPage
