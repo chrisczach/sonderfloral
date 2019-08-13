6da5f118adab5835728a82f8745d6748bb1d9457
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-08-13 14:20:01 -0700

    update 404

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -2,15 +2,23 @@ import React from 'react'
 import SEO from '../components/seo'
 import Container from '../components/layout'
 import Layout from '../containers/layout'
+import { Link } from 'gatsby'
 
 import { responsiveTitle1 } from '../components/typography.module.css'
 
 const NotFoundPage = () => (
   <>
     <SEO title="404: Not found" />
-
-    <h1 className={responsiveTitle1}>Not found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness. </p>
+    <Container>
+      <h1 className={responsiveTitle1}>Page not found</h1>
+      <p>
+        We couldn't find the page you were looking for. This is either because there is an error in
+        the URL entered into your web browser or the page that you're looking for has been moved or
+        deleted.
+      </p>
+      Please double check the url and try again or <Link to="/">click here</Link> to return to the
+      homepage.
+    </Container>
   </>
 )
 
