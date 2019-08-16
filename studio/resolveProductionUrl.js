5597e551536719652ebd7782630e344ff5fb03af
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-08-16 15:11:35 -0700

    test

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -1,28 +1,29 @@
-export default function resolveProductionUrl( document ) {
+export default function resolveProductionUrl(document) {
   const { _type } = document;
 
   let path = "";
-  if ( _type === "category" ) {
+  if (_type === "category") {
     const {
       slug: { current }
     } = document;
-    path = `equipment/${ current }`;
-  } else if ( _type === "page" ) {
+    path = `equipment/${current}`;
+  } else if (_type === "page") {
     const { _id } = document;
-    if ( _id.endsWith( "home" ) ) {
+    if (_id.endsWith("home")) {
       path = "";
-    } else if ( _id.endsWith( "about" ) ) {
+    } else if (_id.endsWith("about")) {
       path = "about";
-    } else if ( _id.endsWith( "contact" ) ) {
+    } else if (_id.endsWith("contact")) {
       path = "contact";
-    } else if ( _type === "project" ) {
+    }
+  } else if (_type === "project") {
     const {
       slug: { current },
       categories: { _ref }
     } = document;
 
-    path = `preview?category=${ _ref }&project=${ current }`;
+    path = `preview?category=${_ref}&project=${current}`;
   }
 
-  return `https://sonderpreview.herokuapp.com/${ path }`;
+  return `https://sonderpreview.herokuapp.com/${path}`;
 }
