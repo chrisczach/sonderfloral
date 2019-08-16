f02ffa74f1138a6a6125a49747b9a1fd013bff0d
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-08-16 15:11:11 -0700

    try again

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -1,3 +1,28 @@
-export default function resolveProductionUrl(document) {
-  return `https://sonderfloral.netlify.com/preview?document=${document._id}`;
+export default function resolveProductionUrl( document ) {
+  const { _type } = document;
+
+  let path = "";
+  if ( _type === "category" ) {
+    const {
+      slug: { current }
+    } = document;
+    path = `equipment/${ current }`;
+  } else if ( _type === "page" ) {
+    const { _id } = document;
+    if ( _id.endsWith( "home" ) ) {
+      path = "";
+    } else if ( _id.endsWith( "about" ) ) {
+      path = "about";
+    } else if ( _id.endsWith( "contact" ) ) {
+      path = "contact";
+    } else if ( _type === "project" ) {
+    const {
+      slug: { current },
+      categories: { _ref }
+    } = document;
+
+    path = `preview?category=${ _ref }&project=${ current }`;
+  }
+
+  return `https://sonderpreview.herokuapp.com/${ path }`;
 }
