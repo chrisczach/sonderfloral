f11effaae1da06b4002f7b7ee2b3a8444e219d88
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-08-20 15:03:27 -0700

    fix modal

--- web/src/pages/portfolio.js
+++ web/src/pages/portfolio.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { graphql } from 'gatsby'
 import { mapEdgesToNodes } from '../lib/helpers'
 import GraphQLErrorList from '../components/graphql-error-list'
@@ -7,6 +7,8 @@ import PortfolioMasonryGrid from '../components/portfolio-masonry-grid'
 import { responsiveTitle1 } from '../components/typography.module.css'
 import styles from './portfolio.module.css'
 import Container from '../components/container'
+import PortfolioMasonryModal from '../components/portfolio-masonry-modal'
+
 export const query = graphql`
   query PortfolioPageQuery {
     posts: allSanityPost(sort: { fields: [_createdAt], order: DESC }) {
@@ -49,13 +51,42 @@ const PortfolioPage = props => {
 
   const postNodes = data && data.posts && mapEdgesToNodes(data.posts)
 
+  const [modalShown, setModal] = useState(false)
+  const [modalImage, setModalImage] = useState()
+  const [aspect, setAspect] = useState(1)
+  const toggleModalOn = ({ mainImage, columns, rows }) => () => {
+    setModal(true)
+    setModalImage(mainImage)
+    setAspect(columns / rows || 1)
+  }
+
   return (
     <>
       <SEO title="Portfolio" />
+      <PortfolioMasonryModal
+        modalShown={modalShown}
+        aspect={aspect}
+        toggleModal={() => setModal(!modalShown)}
+        modalImage={modalImage}
+        setModalImage={setModalImage}
+      />
       <Container>
         <div style={{ minHeight: '100vh', padding: '.5em' }}>
           <h1 className={`${responsiveTitle1} ${styles.headerText}`}>Portfolio</h1>
-          {postNodes && postNodes.length > 0 && <PortfolioMasonryGrid nodes={postNodes} />}
+          {postNodes && postNodes.length > 0 && (
+            <PortfolioMasonryGrid
+              nodes={postNodes}
+              {...{
+                modalShown,
+                setModal,
+                modalImage,
+                setModalImage,
+                aspect,
+                setAspect,
+                toggleModalOn
+              }}
+            />
+          )}
         </div>
       </Container>
     </>
