89835478c83dbd85ffbab6861fe1420ae7ca8b3f
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-09-24 12:45:34 -0700

    fix footer on project pages

--- web/src/components/project.js
+++ web/src/components/project.js
@@ -34,55 +34,58 @@ function Project(props) {
   const portrait = windowHeight > width
 
   return (
-    <article className={styles.root}>
-      {!portrait && (
-        <Link className={styles.categoryLink} to={`/projects/${categorySlug}/`}>
-          ← back to {categoryTitle}
-        </Link>
-      )}
-      {props.mainImage && mainImage.asset && (
-        <div className={styles.imageWrapper} style={{ opacity: position > windowHeight ? 0 : 1 }}>
-          {listener}
-          <Image
-            asset={mainImage}
-            alt={mainImage.alt}
-            args={{ maxWidth: width || 1200, maxHeight: height || 1200 }}
-          />
-        </div>
-      )}
-      <div className={styles.backgroundOverlay} style={{}} />
-      <Container className={styles.container}>
-        <div className={styles.grid}>
-          <div className={styles.mainContent}>
-            <h1 className={`${responsiveTitle1} ${styles.title}`}>{title}</h1>
-            {_rawBody && <BlockContent blocks={_rawBody || []} />}
+    <>
+      <article className={styles.root}>
+        {!portrait && (
+          <Link className={styles.categoryLink} to={`/projects/${categorySlug}/`}>
+            ← back to {categoryTitle}
+          </Link>
+        )}
+        {props.mainImage && mainImage.asset && (
+          <div className={styles.imageWrapper} style={{ opacity: position > windowHeight ? 0 : 1 }}>
+            {listener}
+            <Image
+              asset={mainImage}
+              alt={mainImage.alt}
+              args={{ maxWidth: width || 1200, maxHeight: height || 1200 }}
+            />
           </div>
-          <aside className={styles.metaContent}>
-            {publishedAt && (
-              <div className={styles.publishedAt}>
-                Published{' '}
-                {differenceInDays(new Date(), new Date(publishedAt)) < 3
-                  ? formatDistance(new Date(publishedAt), new Date()) + ' ago'
-                  : `on ` + format(new Date(publishedAt), 'MMMM do yyyy')}
-              </div>
-            )}
-            {members && <RoleList items={members} title="Contributors" />}
-            {relatedProjects && relatedProjects.length > 0 && (
-              <div className={styles.relatedProjects}>
-                <h3 className={styles.relatedProjectsHeadline}>Related projects</h3>
-                <ul>
-                  {relatedProjects.map(project => (
-                    <li key={`related_${project._id}`}>
-                      <Link to={`/project/${project.slug.current}/`}>{project.title}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-          </aside>
-        </div>
-      </Container>
-    </article>
+        )}
+        <div className={styles.backgroundOverlay} style={{}} />
+        <Container className={styles.container}>
+          <div className={styles.grid}>
+            <div className={styles.mainContent}>
+              <h1 className={`${responsiveTitle1} ${styles.title}`}>{title}</h1>
+              {_rawBody && <BlockContent blocks={_rawBody || []} />}
+            </div>
+            <aside className={styles.metaContent}>
+              {publishedAt && (
+                <div className={styles.publishedAt}>
+                  Published{' '}
+                  {differenceInDays(new Date(), new Date(publishedAt)) < 3
+                    ? formatDistance(new Date(publishedAt), new Date()) + ' ago'
+                    : `on ` + format(new Date(publishedAt), 'MMMM do yyyy')}
+                </div>
+              )}
+              {members && <RoleList items={members} title="Contributors" />}
+              {relatedProjects && relatedProjects.length > 0 && (
+                <div className={styles.relatedProjects}>
+                  <h3 className={styles.relatedProjectsHeadline}>Related projects</h3>
+                  <ul>
+                    {relatedProjects.map(project => (
+                      <li key={`related_${project._id}`}>
+                        <Link to={`/project/${project.slug.current}/`}>{project.title}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </aside>
+          </div>
+        </Container>
+      </article>
+      <div className={styles.spacer} />
+    </>
   )
 }
 
